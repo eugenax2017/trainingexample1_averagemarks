ce1dbf3338a42644924b0ad9fbea05d528a501be
linkbub <jose.freire@linkbub.com>
2019-12-02 11:44:06 +0100

    Depdency Injection

---
+++ Ejemplos Patrones/DependencyInjection/Core/Core.Lib/Infrastructure/Interfaces/IDependencyContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Lib.Infrastructure
+{
+    public interface IDependencyContainer
+    {
+        void Register<Tin, Tout>();
+
+        Tin Resolve<Tin>();
+    }
+}
---
+++ Ejemplos Patrones/DependencyInjection/Core/Core.Lib/Infrastructure/SimpleDependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Lib.Infrastructure
+{
+    public class SimpleDependencyContainer : IDependencyContainer
+    {
+        public void Register<Tin, Tout>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tin Resolve<Tin>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
